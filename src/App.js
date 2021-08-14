7acb42d7e8260d3f2e2ca94240cd06df02982834
AloofSkip <nic.veloce@gmail.com>
2021-08-14 15:43:59 -0700

    Server connectd to client!

--- src/App.js
+++ src/App.js
@@ -6,49 +6,27 @@ import SearchBar from "./components/searchBar";
 
 class App extends Component {
   state = {
-    results: {
-      BAC001: {
-        specification: "BAC001",
-        revision: "PREFY",
-        title: "PREFACE",
-        date: "18-FEB-2021",
-      },
-      BAC5000: {
-        specification: "BAC5000",
-        revision: "AW",
-        title: "SEALING, GENERAL",
-        date: "10-MAY-2021",
-      },
-      "BAC5000-1": {
-        specification: "BAC5000-1",
-        revision: "B",
-        title: "REPACKAGING AND FREEZING OF SEALANT BY END USERS",
-        date: "27-JUN-2019",
-      },
-      BAC5001: {
-        specification: "BAC5001",
-        revision: "M",
-        title: "FLUID LINES, FITTINGS AND PRESSURE TESTING",
-        date: "30-SEP-2013",
-      },
-      "BAC5001-1": {
-        specification: "BAC5001-1",
-        revision: "H",
-        title: "CUT OFF, END FORMING, HANDLING AND MARKING OF TUBES AND DUCTS",
-        date: "05-JUL-2019",
-      },
-    },
+    results: {},
   };
 
   updateResults(newResults) {
-    let newState = [...this.state];
+    let newState = { ...this.state };
     newState["results"] = newResults;
     this.setState(newState);
   }
 
   getResults = (e) => {
     e.preventDefault();
-    console.log("Get Results!");
+
+    const search_string = document.getElementById("spec-input").value;
+
+    if (search_string == "") return None;
+
+    const uri = "/specs/" + search_string;
+
+    fetch(uri)
+      .then((response) => response.json())
+      .then((results) => this.updateResults(results));
   };
 
   render() {
--- webpack.config.js
+++ webpack.config.js
@@ -10,25 +10,35 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /(node_modules|bower_components)/,
         loader: "babel-loader",
-        options: { presets: ["@babel/env"] }
+        options: { presets: ["@babel/env"] },
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
+        use: ["style-loader", "css-loader"],
+      },
+    ],
   },
   resolve: { extensions: ["*", ".js", ".jsx"] },
   output: {
     path: path.resolve(__dirname, "dist/"),
     publicPath: "/dist/",
-    filename: "bundle.js"
+    filename: "bundle.js",
   },
+  devtool: "inline-source-map",
   devServer: {
     contentBase: path.join(__dirname, "public/"),
     port: 3000,
     publicPath: "http://localhost:3000/dist/",
-    hotOnly: true
+    hotOnly: true,
+    proxy: {
+      "/specs/**": "http://localhost:5000",
+    },
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers":
+        "X-Requested-With, content-type, Authorization",
+    },
   },
-  plugins: [new webpack.HotModuleReplacementPlugin()]
+  plugins: [new webpack.HotModuleReplacementPlugin()],
 };
